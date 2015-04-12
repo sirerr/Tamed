b7b84d63cc7f5ad7ddd94fb09e0c03b81796babb
sirerr <sirerr@gmail.com>
2015-04-12 01:55:35 -0400

    complete in a way

---
+++ unity/Assets/Scripts/Area0scripts/area0manager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class area0manager : MonoBehaviour {
+
+	public float timeuntilstart = 0;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(talkthenstart());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator talkthenstart()
+	{
+		yield return new WaitForSeconds(timeuntilstart);
+		Application.LoadLevel("area1");
+
+	}
+}
