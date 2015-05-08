feaf75a707e6b60f197fbdb8f1192ebf210bc2e1
sirerr <sirerr@gmail.com>
2015-05-07 21:50:32 -0400

    working on entrance of game

--- unity/Assets/Scripts/Area0scripts/area0manager.cs
+++ unity/Assets/Scripts/Area0scripts/area0manager.cs
@@ -3,7 +3,15 @@ using System.Collections;
 
 public class area0manager : MonoBehaviour {
 
+	public float part1time = 0;
 	public float timeuntilstart = 0;
+	public ParticleSystem sparkps;
+	public GameObject sparkobj;
+
+	//eye quad
+	public GameObject floatingeye;
+	public float maincolorlow = 0;
+	public float maincolorhigh =0;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +25,18 @@ public class area0manager : MonoBehaviour {
 
 	IEnumerator talkthenstart()
 	{
+		yield return new WaitForSeconds(part1time);
+		sparkobj.SetActive(true);
+		floatingeye.SetActive(true);
+
+		sparkps.Play();
+		yield return new WaitForSeconds(1f);
+		floatingeye.SetActive(false);
+		yield return new WaitForSeconds(2f);
+		//particle system
+		sparkobj.SetActive(true);
+		sparkps.Play();
+		floatingeye.SetActive(true);
 		yield return new WaitForSeconds(timeuntilstart);
 		Application.LoadLevel("area1");
 
